b5e280bb00ab962a892c16ba749a8346912cb7c1
Ryan C. Thompson <rct@thompsonclan.org>
2016-07-12 14:45:12 -0700

    Remove normalization testing from csaw-analysis.R
    That now lives in a separate file

--- csaw-analysis.R
+++ csaw-analysis.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 
 library(stringr)
 library(magrittr)
@@ -7,7 +8,6 @@ options(mc.cores=parallel::detectCores())
 registerDoParallel(cores=parallel::detectCores())
 library(SummarizedExperiment)
 library(readr)
-library(dplyr)
 library(edgeR)
 library(limma)
 library(csaw)
@@ -17,6 +17,8 @@ library(rtracklayer)
 library(RColorBrewer)
 library(reshape2)
 library(parallel)
+library(ks)
+library(dplyr)
 
 ## Ensure output directory exists
 dir.create("results/csaw", FALSE, TRUE)
@@ -26,9 +28,9 @@ tsmsg <- function(...) {
 }
 
 suppressPlot <- function(arg) {
-    dev.new()
+    png("/dev/null")
+    on.exit(dev.off())
     result <- arg
-    dev.off()
     result
 }
 
@@ -249,216 +251,38 @@ in.forked.process <- function(expr) {
   }
 }
 
-tsmsg("Reading ChIP input data")
-input.bigbin.counts <- readRDS(sprintf("saved_data/bigbin-counts-%s.RDS", "input"))
-input.window.counts <- readRDS(sprintf("saved_data/window-counts-%s.RDS", "input"))
-
-comp.inputnorm <- normOffsets(input.bigbin.counts, type="scaling")
-
 chips <- c("H3K4me3", "H3K4me2", "H3K27me3")
 
-## Read peak files
-peaks <- lapply(
-    setNames(nm=chips),
-    . %>% sprintf(fmt="data_files/ChIP-Seq/%s_peaks_IDR_filtered.bed") %>%
-    import.narrowPeak)
-
-peaks %>% sapply(. %>% width %>% sum %>% divide_by(73) %>% round)
+ave.count.threshold <- 5
 
 for (chip in chips) { in.forked.process({
     tsmsg("Reading data for ", chip)
-    bigbin.counts <- readRDS(sprintf("saved_data/bigbin-counts-%s.RDS", chip))
-    window.counts <- readRDS(sprintf("saved_data/window-counts-%s.RDS", chip))
-    chip.peaks <- peaks[[chip]]
-    ## Filter to windows with at least 10 counts total
-    keep <- rowSums(assay(window.counts)) >= 10
-    window.counts %<>% .[keep,]
-    invisible(gc())
+    ## Let's avoid re-doing all the normalization and filtering by
+    ## loading the saved data from csaw-qc.R
+    dge <- readRDS(sprintf("saved_data/csaw-DGEList-%s.RDS", chip))
+    dge <- dge[,dge$samples %$% order(ChIP, Celltype, Day, Donor, Sample)]
+    chip.peaks <- import.narrowPeak(
+        sprintf(fmt="data_files/ChIP-Seq/%s_peaks_IDR_filtered.bed", chip))
 
-    tsmsg("Counting number of non-zero bins")
-    colData(window.counts)$NonZeroBins <- withGC(colSums(assay(window.counts) > 0))
+    tsmsg("Filtering")
+    ab <- dge$genes$Abundance
+    thresh <- aveLogCPM(5, lib.size=mean(dge$samples$totals))
+    present <- ab >= thresh
+    dge <- dge[present,]
 
+    ## Use efficiency normalization
+    dge$samples %<>% mutate(norm.factors=PeakNormFactors)
 
-    ## Compute background normalization from big bins
-    tsmsg("Computing composition normalization from background")
-    colData(window.counts)$CompNormFactors <- normOffsets(bigbin.counts, type="scaling")
+    design <- model.matrix(~0 + TreatmentGroup + Donor, dge$samples)
+    colnames(design) %<>%
+        str_replace("^TreatmentGroup", "") %>%
+        str_replace("^DonorDn", "Donor")
 
-    tsmsg("Computing average window abundances")
-    abundances <- withGC(aveLogCPM(asDGEList(window.counts)))
+    tsmsg("Estimating dispersions")
+    dge %<>% estimateDisp(design, robust=TRUE)
+    fit <- glmQLFit(dge.comp, design, robust=TRUE)
 
-    tsmsg("Computing window filter statistic")
-    ## Compute enirchment of windows above average global background
-    filter.stat <- filterWindows(window.counts, bigbin.counts, type="global")
-
-    filter.stat$global.bg <- filter.stat %$% {abundances - filter} %>% mean
-    filter.stat.df <- filter.stat %$% {
-        rbind(
-            data.frame(BinType="ChIP", Abundance=abundances %>% quantile(seq(0, 1, length.out=1e6))),
-            data.frame(BinType="BG", Abundance=back.abundances))
-    }
-    filter.stat.thresholds <- data.frame(Line=c("Background", "Threshold"),
-                                         Abundance=filter.stat$global.bg + c(0, log2(3)))
-
-    ## Pick a threshold
-    filter.threshold <- filter.stat$global.bg + log2(3)
-
-    tsmsg("plotting abundance vs peak overlap")
-    ## Plot abundance vs peak containment
-    keep.table <- data.frame(
-        PeakKeep=overlapsAny(window.counts, GRangesList(list(chip.peaks))),
-        AbundanceKeep=abundances >= filter.threshold,
-        Abundance=abundances)
-
-    tsmsg("Computing efficiency normalization from high-abundance windows")
-    high.ab.window.counts <- window.counts[abundances >= filter.threshold,]
-    colData(window.counts)$HANormFactors <- normOffsets(high.ab.window.counts, type="scaling")
-
-    table(keep.table[c("PeakKeep", "AbundanceKeep")])
-
-    {
-        p <- ggplot(keep.table) +
-            aes(y=Abundance, x=PeakKeep) +
-            geom_violin() +
-            xlab("Peak Overlap") +
-            ggtitle("Window Mean logCPM by Peak Status")
-        pdf(sprintf("results/csaw/%s-window-abundance-vs-peaks.pdf", chip))
-        print(p)
-        dev.off()
-    }
-
-    ## Based on above plot, we decide to use the peaks as filter
-    ## criterion
-    peak.overlap <- overlapsAny(window.counts, chip.peaks)
-    peak.window.counts <- window.counts[peak.overlap,]
-    tsmsg("Computing efficiency normalization from peak windows")
-    pnf <- normOffsets(peak.window.counts, type="scaling")
-    colData(window.counts)$
-    PeakNormFactors <- pnf
-    colData(peak.window.counts)$PeakNormFactors <- pnf
-
-    ## Create DGEList and populate $genes and $samples
-    dge <- asDGEList(window.counts)
-    ## Saves memory
-    rownames(dge$counts) <- NULL
-    gc()
-    dge$genes <- rowRanges(window.counts) %>% as.data.frame %>% lapply(Rle) %>% DataFrame
-    dge$samples %<>% cbind(colData(window.counts)) %>% as.data.frame
-    dge$samples$nf.logratio <- dge$samples %$% log2(PeakNormFactors / CompNormFactors)
-
-    {
-        tsmsg("Testing norm factors for association with experimental factors")
-        xvars <- c("Celltype", "Day", "Donor", "TreatmentGroup")
-        yvars <- c("lib.size", "CompNormFactors", "HANormFactors", "PeakNormFactors", "nf.logratio")
-
-        formulas <- list()
-        for (xv in xvars) {
-            for (yv in yvars) {
-                modname <- sprintf("%s.vs.%s", yv, xv)
-                formulas[[modname]] <- as.formula(sprintf("%s ~ %s", yv, xv))
-            }
-        }
-
-        mods <- lapply(formulas, . %>% lm(data=dge$samples))
-        tests <- mods %>% lapply(anova)
-        results <- tests %>% sapply(. %$% `Pr(>F)` %>% .[1]) %>% data_frame(Test=names(.) , PValue=., FDR=p.adjust(., "BH")) %>% arrange(PValue, FDR)
-        write.xlsx(results, sprintf("results/csaw/%s-normfactor-tests.xlsx", chip))
-    }
-
-    {
-        tsmsg("Plotting norm factors vs experimental factors")
-        ## Plot norm factors vs lib sizes
-        nf.vs.ls <- dge$samples %>% select(LibSize=lib.size, HiAb=HANormFactors, Peak=PeakNormFactors, Comp=CompNormFactors) %>%
-            melt(id.vars="LibSize", variable.name = "NormType", value.name="NormFactor")
-        p <- list(ggplot(nf.vs.ls) +
-                  aes(x=LibSize, y=NormFactor, group=NormType, color=NormType, fill=NormType) +
-                  geom_point() + geom_smooth(alpha=0.15) +
-                  scale_x_continuous(trans=log_trans(2)) +
-                  scale_y_continuous(trans=log_trans(2)) +
-                  ggtitle("Norm Factors vs Library Sizes"))
-        ## Plot lib sizes & norm factors vs experimental factors
-        ls.nf.vs.exp <- dge$samples %>%
-            select(
-                Sample, Celltype, Day, Donor,
-                Group=TreatmentGroup, LibSize=lib.size,
-                HighAbundanceNormFactor=HANormFactors,
-                PeakNormFactor=PeakNormFactors,
-                CompositionNormFactor=CompNormFactors) %>%
-            melt(id.vars=c("Sample", "Celltype", "Day", "Donor",
-                           "Group"),
-                 variable.name="Factor", value.name="Value")
-        p0 <- ggplot(ls.nf.vs.exp) +
-            aes(x=Value) + scale_x_continuous(trans=log_trans(2)) +
-            geom_point(position=position_jitter(height=0.25)) +
-            facet_wrap(~Factor, scales="free_x")
-        vars.to.plot <- c("Celltype", "Day", "Donor", "Group")
-        for (i in vars.to.plot) {
-            p <- c(p, list(p0 + aes_string(y=i) +
-                           ggtitle(sprintf("Library Sizes and Norm Factors vs %s", i))))
-        }
-        pdf(sprintf("results/csaw/%s-normfactors.pdf", chip))
-        print(p)
-        dev.off()
-    }
-
-    ## Select the most and least extreme samples for plotting MA plots
-
-    middle.samples <- dge$samples$nf.logratio %>% abs %>% order
-    cn.higher.samples <- dge$samples$nf.logratio %>% order
-    pn.higher.samples <- rev(cn.higher.samples)
-
-    tsmsg("Computing logCPM")
-    logcpm <- cpm(dge, log=TRUE)
-
-    doMAPlot <- function(s1, s2) {
-        pointdata <- data.frame(S1=logcpm[,s1], S2=logcpm[,s2]) %>%
-            transmute(A=(S1+S2)/2, M=S2-S1) %>%
-            filter(A >= -2)
-        bw <- sapply(pointdata[c("A", "M")], . %>% (MASS::bandwidth.nrd))
-        dataranges <- sapply(pointdata[c("A", "M")], . %>% range %>% diff)
-
-        ## Use the same perceptual bandwidth in both dimensions
-        ## (yielding circular blobs)
-        bw <- min(bw / c(1,2)) * c(1,2)
-
-        linedata <- c(Comp="CompNormFactors",
-                      Peaks="PeakNormFactors",
-                      HiAb="HANormFactors") %>%
-            sapply(. %>% dge$samples[[.]] %>% log2 %>% {.[s2] - .[s1]}) %>%
-            data.frame(NormFactor=., NormType=names(.))
-
-        ggplot(pointdata) +
-            aes(x=A, y=M) +
-            stat_density2d(contour=FALSE, geom="raster", n=512, interpolate=TRUE,
-                           aes(fill=..density.., alpha=..density..)) +
-            scale_fill_gradientn(colors=suppressWarnings(brewer.pal(Inf, "Blues")),
-                                 trans=power_trans(1/8),
-                                 name="Density") +
-            scale_alpha_continuous(trans=power_trans(1/20), guide=FALSE) +
-            geom_hline(data=linedata, aes(yintercept=NormFactor, color=NormType)) +
-            scale_color_discrete(name="Norm Type") +
-            scale_x_continuous(limits=pointdata$A %>% range %>% expand_range(add=bw[1] * 3),
-                               expand=c(0,0)) +
-            scale_y_continuous(limits=pointdata$M %>% range %>% expand_range(add=bw[2] * 3),
-                               expand=c(0,0)) +
-            coord_fixed(0.5)
-    }
-    p <- seq_len(floor(length(cn.higher.samples) / 2)) %>%
-        lapply(function(i) {
-            s1 <- cn.higher.samples[i]
-            s2 <- cn.higher.samples[length(cn.higher.samples) - i + 1]
-            title <- sprintf("MA Plot for %s vs %s",
-                             colnames(dge)[s1], colnames(dge)[s2])
-            tsmsg("Making ", title)
-            withGC(doMAPlot(s1, s2) +
-                   ggtitle(title))
-        })
-    {
-        tsmsg("Printing MA plots")
-        pdf(sprintf("results/csaw/%s Selected Sample MA Plots.pdf", chip), width=10, height=10)
-        withGC(print(p))
-        dev.off()
-    }
     tsmsg("Saving image")
-    save.image(sprintf("csaw-%s.rda", chip))
+    save.image(sprintf("saved_data/csaw-norm-eval-%s.rda", chip))
     NULL
 })}
