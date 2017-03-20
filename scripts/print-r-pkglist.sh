e369477db3cf8f079e9477566e653487a6305301
Ryan C. Thompson <rct@thompsonclan.org>
2017-03-20 16:31:10 -0700

    Look in Rmd files for R package deps
    The script for listing all R package dependencies now looks in Rmd
    files in addition to R scripts.

--- scripts/print-r-pkglist.sh
+++ scripts/print-r-pkglist.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 scripts_dir="$(dirname "$0")"
-cat "$scripts_dir"/*.R | \
+cat "$scripts_dir"/*.R{,md} | \
     grep -v 'character\.only' | \
     perl -lane 'print $1 if m{library\(([A-Za-z0-9._]+)}' | \
     sort -u
